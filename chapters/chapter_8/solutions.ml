193067c6781fc6fbcccbcd9367f9603da2dc889d
Mario Nebl <hello+github.com@mario-nebl.de>
2019-05-28 07:19:28 +0000

    Implement 8.1 and 8.2

--- chapters/chapter_8/solutions.ml
+++ chapters/chapter_8/solutions.ml
@@ -1,3 +1,4 @@
+(* Chapter 8 - Looking Things up, p. 61 - 64 *)
 let first (a, _) = a
 let second (_, a) = a
 
@@ -9,5 +10,34 @@ let rec lookup key = function
 
 let rec add k v = function
   | [] -> [(k, v)]
-  | (k', v')::t when k' == k -> (k, v) :: t
+  | (k', _)::t when k' == k -> (k, v) :: t
   | (k', v')::t when k' <> k -> (k', v') :: add k v t
+  | _ -> failwith "unreachable"
+
+let rec remove k = function
+  | [] -> []
+  | (k', _)::t when k' == k -> t
+  | (k', v')::t when k' <> k -> (k', v') :: remove k t
+  |  _ -> failwith "unreachable"
+
+let exists k d =
+  match lookup k d with
+  | None -> false
+  | Some(_) -> true
+
+(* 1. Write a function to determine the number of different keys in a dictionary *)
+let distinct d =
+  let rec distinct' s = function
+  | [] -> s
+  | (k, _)::t ->
+    let s' = if List.exists (fun k' -> k' == k) s then s else k :: s in
+    distinct' s' t
+  in
+  distinct' [] d |> List.length
+
+(* 2. Write a function replace which is like add, but raises Not_found if the key is not already there *)
+let rec replace k v = function
+  | [] -> raise Not_found
+  | (k', _)::t when k == k' -> (k, v) :: t
+  | (k', v')::t when k <> k' -> (k', v') :: replace k v t
+  | _ -> failwith "unreachable"
--- chapters/chapter_8/test.ml
+++ chapters/chapter_8/test.ml
@@ -25,6 +25,36 @@ let tests =
     (fun _ -> assert_equal [(1, 2); (2, 3)] (add 2 3 [(1, 2)]));
     "add 2 4 [(1, 2), (2, 3)]">::
     (fun _ -> assert_equal [(1, 2); (2, 4)] (add 2 4 [(1, 2); (2, 3)]));
+    "remove None []">::
+    (fun _ -> assert_equal [] (remove None []));
+    "remove 1 [(2, 1)]">::
+    (fun _ -> assert_equal [(2, 1)] (remove 1 [(2, 1)]));
+    "remove 2 [(2, 1)]">::
+    (fun _ -> assert_equal [] (remove 2 [(2, 1)]));
+    "remove 1 [(2, 1); (1, 1); (3, 1)]">::
+    (fun _ -> assert_equal [(2, 1); (3, 1)] (remove 1 [(2, 1); (1, 1); (3, 1)]));
+    "exists 1 [(1, 1)]">::
+    (fun _ -> assert_equal true (exists 1 [(1, 1)]));
+    "exists 2 [(1, 1), (2, 1)]">::
+    (fun _ -> assert_equal true (exists 2 [(1, 1); (2, 1)]));
+    "exists 3 [(1, 1), (2, 1)]">::
+    (fun _ -> assert_equal false (exists 3 [(1, 1); (2, 1)]));
+    "exists None []">::
+    (fun _ -> assert_equal false (exists None []));
+    "distinct []">::
+    (fun _ -> assert_equal 0 (distinct []));
+    "distinct [(1, 1)]">::
+    (fun _ -> assert_equal 1 (distinct [(1, 1)]));
+    "distinct [(1, 1); (2, 1)]">::
+    (fun _ -> assert_equal 2 (distinct [(1, 1); (2, 1)]));
+    "distinct [(1, 1); (3, 1); (2, 1); (3, 1)]">::
+    (fun _ -> assert_equal 3 (distinct [(1, 1); (3, 1); (2, 1); (3, 1)]));
+    "replace 1 2 []">::
+    (fun _ -> assert_equal (Error "Not found") (try Ok (replace 1 2 []) with Not_found -> Error "Not found"));
+    "replace 2 3 [(1, 2)]">::
+    (fun _ -> assert_equal (Error "Not found") (try Ok (replace 2 3 []) with Not_found -> Error "Not found"));
+    "replace 2 4 [(1, 2); (2, 3)]">::
+    (fun _ -> assert_equal (Ok [(1, 2); (2, 4)]) (try Ok (replace 2 4 [(1, 2); (2, 3)]) with Not_found -> Error "Not found"));
   ]
 
 let () =
