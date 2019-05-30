0ce88a88ad01a62e854d09b1e0fb1e79e5d07916
Mario Nebl <hello+github.com@mario-nebl.de>
2019-05-30 20:21:53 +0000

    Complete 10.5

--- chapters/chapter_10/solutions.ml
+++ chapters/chapter_10/solutions.ml
@@ -31,5 +31,33 @@ let stack rs = rs
   |> List.sort (fun a b -> x a - x b)
 
 (* 5. Write take, drop and fuctions for the sequence type *)
+module Seq = struct
+  type 'a t = Nil | Cons of 'a * 'a t
+
+  let rec to_list = function
+    | Nil -> []
+    | Cons(h, t) -> h :: (to_list t)
+
+  let rec of_list = function
+    | [] -> Nil
+    | h::t -> Cons(h, of_list t)
+
+  let rec take n = function
+    | Nil when n > 0 -> failwith "end of list"
+    | Cons(_) when n = 0 -> Nil
+    | Nil -> Nil
+    | Cons(h, t) -> Cons (h, take (n - 1) t)
+
+  let rec drop n = function
+    | Nil when n > 0 -> failwith "end of list"
+    | Nil -> Nil
+    | Cons(h, t) when n = 0 -> Cons(h, t)
+    | Cons(_, t) -> drop (n - 1) t
+
+  let rec map f = function
+    | Nil -> Nil
+    | Cons(h, t) -> Cons(f h, map f t)
+end
+
 (* 6. Extend the expr type and the evaluate function to allow raising a number to a power *)
 (* 7. Use the option type to deal with the problem that Division_by_zero may be raised from the evaluate function *)
--- chapters/chapter_10/test.ml
+++ chapters/chapter_10/test.ml
@@ -5,10 +5,13 @@ let ppoint (x, y) = Printf.sprintf "(%i, %i)" x y
 let psquare p l = Printf.sprintf "Square(%s, %i)" (ppoint p) l
 let prectangle a b = Printf.sprintf "Rect(%s, %s)" (ppoint a) (ppoint b)
 let prect r = match r with | Square(p, l) -> psquare p l | Rectangle(a, b) -> prectangle a b
+let plist i l = l |> List.map i |> String.concat "; " |> Printf.sprintf "[%s]"
+let pseq i s = Seq.to_list s |> plist i
 
 let ai exp act __ctx = assert_equal exp act ~printer:string_of_int
 let ar exp act __ctx = assert_equal exp act ~printer:prect
-let alr exp act __ctx = assert_equal exp act ~printer:(fun l -> List.map prect l |> String.concat "; ")
+let alr exp act __ctx = assert_equal exp act ~printer:(plist prect)
+let aseq exp act __ctx = assert_equal exp act ~printer:(pseq string_of_int)
 
 let tests =
   [
@@ -36,6 +39,20 @@ let tests =
     (ar (Rectangle ((0, 0), (-2, -3))) (erect (Rectangle ((0, 0), (3, 2)))));
     "stack [Rectangle ((0, 0), (5, 3)); Square ((0, 0), 2); Rectangle ((0, 0), (1, 2))]">::
     (alr [Rectangle ((0, 0), (1, 2)); Square ((0, 0), 2); Rectangle ((0, 0), (-3, -5))] (stack [Rectangle ((0, 0), (5, 3)); Square ((0, 0), 2); Rectangle ((0, 0), (1, 2))]));
+    "take 1 Cons (1, (Cons (2, Nil))) -> Cons (1, Nil)">::
+    (aseq (Seq.of_list [1]) (Seq.take 1 (Seq.of_list [1; 2])));
+    "take 2 Cons (1, (Cons (2, Nil))) -> Cons (1, (Cons (2, Nil)))">::
+    (aseq (Seq.of_list [1; 2]) (Seq.take 2 (Seq.of_list [1; 2])));
+    "take 1 Nil -> fails">::
+    (fun _ -> assert_equal (Error "end of list") (try Ok (Seq.take 1 Nil) with _ -> Error "end of list")); 
+    "drop 1 Cons (1, (Cons (2, Nil))) -> Cons (2, Nil)">::
+    (aseq (Seq.of_list [2]) (Seq.drop 1 (Seq.of_list [1; 2])));
+    "drop 2 Cons (1, (Cons (2, Nil))) -> Nil">::
+    (aseq Nil (Seq.drop 2 (Seq.of_list [1; 2])));
+    "drop 1 Nil -> fails">::
+    (fun _ -> assert_equal (Error "end of list") (try Ok (Seq.drop 1 Nil) with _ -> Error "end of list")); 
+    "map ((+) 1) Cons(1, Nil)">::
+    (aseq (Seq.of_list [2]) (Seq.map ((+) 1) (Seq.of_list [1])));
   ]
 
 let () =
